a55f53378ba35e98c33477696f2221120ad05571
Andrew <healeycodes@gmail.com>
2021-07-01 19:35:31 +0100

    Starting with expr2 as a base

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/healeycodes/funlang
 
 go 1.13
 
-require github.com/alecthomas/participle/v2 v2.0.0-alpha6 // indirect
+require (
+	github.com/alecthomas/kong v0.2.17
+	github.com/alecthomas/participle/v2 v2.0.0-alpha6
+	github.com/alecthomas/repr v0.0.0-20181024024818-d37bc2a10ba1
+)
--- parse.go
+++ parse.go
@@ -1,11 +1,83 @@
-package funlang
+package main
 
 import (
+	"strings"
 
+	"github.com/alecthomas/kong"
 	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/repr"
 )
 
-parser, err := participle.Build(&INI{})
+type Program struct {
+	Expression []*Expression `@@*`
+}
 
-ast := &INI{}
-err := parser.ParseString("", "size = 10", ast)
+// type Declaration struct {
+// 	Pos lexer.Position
+
+// 	Type string `@Type`
+// 	Name string `@Ident`
+// }
+
+// type FunctionBody struct {
+// 	Pos lexer.Position
+
+// 	Expression []*Expression `@@*`
+// }
+
+type Expression struct {
+	Equality *Equality `@@`
+}
+
+type Equality struct {
+	Comparison *Comparison `@@`
+	Op         string      `[ @( "!" "=" | "=" "=" )`
+	Next       *Equality   `  @@ ]`
+}
+
+type Comparison struct {
+	Addition *Addition   `@@`
+	Op       string      `[ @( ">" | ">" "=" | "<" | "<" "=" )`
+	Next     *Comparison `  @@ ]`
+}
+
+type Addition struct {
+	Multiplication *Factor   `@@`
+	Op             string    `[ @( "-" | "+" )`
+	Next           *Addition `  @@ ]`
+}
+
+type Factor struct {
+	Unary *Unary  `@@`
+	Op    string  `[ @( "/" | "*" | "^" | "%" | "&" | "|")`
+	Next  *Factor `  @@ ]`
+}
+
+type Unary struct {
+	Op      string   `  ( @( "!" | "-" )`
+	Unary   *Unary   `    @@ )`
+	Primary *Primary `| @@`
+}
+
+type Primary struct {
+	Number        *float64    `  @Float | @Int`
+	String        *string     `| @String`
+	Bool          *bool       `| ( @"true" | "false" )`
+	Nil           bool        `| @"nil"`
+	SubExpression *Expression `| "(" @@ ")" `
+}
+
+var parser = participle.MustBuild(&Program{}, participle.UseLookahead(2))
+
+func main() {
+	var cli struct {
+		Program []string `arg required help:"Expression to parse."`
+	}
+	ctx := kong.Parse(&cli)
+
+	program := &Program{}
+	err := parser.ParseString("", strings.Join(cli.Program, " "), program)
+	ctx.FatalIfErrorf(err)
+
+	repr.Println(program)
+}
