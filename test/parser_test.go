16c6eb650be9ef1d2e703750dc38d4e96ed5cd33
Andrew <healeycodes@gmail.com>
2021-07-06 21:20:01 +0100

    Add tests for the parser

---
+++ test/parser_test.go
@@ -0,0 +1,31 @@
+package golfcart
+
+import (
+	"testing"
+
+	"github.com/healeycodes/golfcart/pkg/golfcart"
+)
+
+func TestGenerateAST(t *testing.T) {
+	program := "a = 1"
+	_, err := golfcart.GenerateAST(program)
+	if err != nil {
+		t.Errorf("GenerateAST: %v", err)
+	}
+}
+
+func TestEval(t *testing.T) {
+	program := "a = 1"
+
+	// Covered by TestGenerateAST
+	ast, _ := golfcart.GenerateAST(program)
+
+	context := golfcart.Context{}
+	context.Init()
+	golfcart.InjectRuntimeFunctions(&context)
+
+	_, err := ast.Eval(&context)
+	if err != nil {
+		t.Errorf("Eval: %v", err)
+	}
+}
